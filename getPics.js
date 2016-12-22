259d0c2affc86b6aec62097d0f98a8c359bf247e
endziu <koper.andrzej@gmail.com>
2016-12-22 22:16:55 +0000

    refactor a bit

--- getPics.js
+++ getPics.js
@@ -1,24 +1,23 @@
 const data = require('./data/tracks.json')
 const fs = require('fs')
 const download = require('./download.js')
-const { map } = require('ramda')
 
 const getPicUrl =
     (obj) => obj.artwork_url === null ? obj.userPic : obj.artwork_url 
 
-const urls = map(getPicUrl, data)
 
-const doDownloads = (url, index) => {
+const doDownload = (url, index) => {
   download(
     url,
     `data/imgs/file${index}.jpeg`,
     (state) => console.log('progress: ', state),
-    (res)=> console.log('response: ', res.statusCode),
-    (e)=> console.log('error: ', e),
-    ()=> console.log('done!')
+    (res) => console.log('response: ', res.statusCode),
+    (e) => console.log('error: ', e),
+    () => console.log('done!')
   )
 }
 
-urls.map(doDownloads)
+data.map(getPicUrl)
+    .map(doDownload)
 
 
